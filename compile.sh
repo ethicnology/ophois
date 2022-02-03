f49dedd0d59b0b88381d88be29440639f86ce9d6
ethicnology <ethicnology@pm.me>
2022-02-03 10:53:14 +0100

    compile libc statically thanks to musl

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker + cargo cross + toolchain target needed
-targets=("aarch64-unknown-linux-gnu" "i686-pc-windows-gnu" "i686-pc-windows-msvc" "i686-unknown-linux-gnu" "x86_64-apple-darwin" "x86_64-pc-windows-gnu" "x86_64-pc-windows-msvc" "x86_64-unknown-linux-gnu")
+targets=("i686-unknown-linux-musl" "x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl")
 
 for target in ${targets[@]}; do
-    eval "cross build --release --target $target " 
+    eval "cross build --release --target=$target " 
 done
